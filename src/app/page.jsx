5d30f05071c81ba0a4db4e8077f547d95647d4c2
boluwatife obateru <boluwatifeobateru@gmail.com>
2025-05-17 23:12:39 -0700

    sxsmx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,16 +3,16 @@ import LoginForm from "./components/loginForm";
 
 export default function Home() {
   return (
-   <section className="w-full ">
-    <div className="absolute w-full ">
-      <img className="w-full  object-cover" src="/background.JPG" alt="" />
+   <section className="w-full h-screen">
+    <div className="absolute w-full h-screen">
+      <img className="w-full h-screen object-cover" src="/background.JPG" alt="" />
     </div>
    <div className="relative w-full h-screen flex justify-center items-center bg-black-100 bg-opacity-180">
    <LoginForm />
    </div>
-   <div className="relative pt-38 bottom-0 w-full   flex justify-center items-center ">
+   {/* <div className="relative pt-25 bottom-0 w-full   flex justify-center items-center ">
    <Foot />
-   </div>
+   </div> */}
    </section>
   );
 }
